9e9ae5d1be5b1d41f2fd5364ca5c52077251126b
warrenniu <warrenniu@gmail.com>
2020-11-15 14:23:48 -0500

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10)
+
+let mod = 32 / 8
+
+let max = Math.max(5, 10, 20)
